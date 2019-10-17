f132e6c6294a4ac69f6695f28aab0ede19037e03
thejbix <jaydonbixenman@hotmail.com>
2019-10-17 10:55:06 -0500

    oauth by client credentials rather than callback
    using jupyter notebooks for quick development and testing of this feature

---
+++ labelme/utils/oauth2.py
@@ -0,0 +1,6 @@
+import oauthlib
+import requests
+
+client_id = 'zLjOtEumL2rurN5YsNMuKRK9PR5wuLPrBGQTSQvmf5U'
+client_secret = 'PJ6aLnVpAC77Ad7-Yg1coVCGm1zbNH1znk8MMWprm8Q'
+redirect_uri = 'cowcounteroauth://connect/callback'
